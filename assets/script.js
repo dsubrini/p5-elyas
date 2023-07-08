60ebd8821df33b27eb10143512441e3bcb3506e0
Dimitri Subrini <dimitri.subrini@gmail.com>
2023-07-08 17:05:14 +0200

    premiere étape

--- assets/script.js
+++ assets/script.js
@@ -1,18 +1,30 @@
+const arrowLeftElement = document.querySelector('.arrow_left');
+const arrowRightElement = document.querySelector('.arrow_right');
+const imgBannerElement = document.querySelector('.banner-img');
+const pBannerElement = document.querySelector('#banner p');
+
 const slides = [
-	{
-		"image":"slide1.jpg",
-		"tagLine":"Impressions tous formats <span>en boutique et en ligne</span>"
-	},
-	{
-		"image":"slide2.jpg",
-		"tagLine":"Tirages haute définition grand format <span>pour vos bureaux et events</span>"
-	},
-	{
-		"image":"slide3.jpg",
-		"tagLine":"Grand choix de couleurs <span>de CMJN aux pantones</span>"
-	},
-	{
-		"image":"slide4.png",
-		"tagLine":"Autocollants <span>avec découpe laser sur mesure</span>"
-	}
-]
+  {
+    image: 'slide1.jpg',
+    tagLine: 'Impressions tous formats <span>en boutique et en ligne</span>',
+  },
+  {
+    image: 'slide2.jpg',
+    tagLine:
+      'Tirages haute définition grand format <span>pour vos bureaux et events</span>',
+  },
+  {
+    image: 'slide3.jpg',
+    tagLine: 'Grand choix de couleurs <span>de CMJN aux pantones</span>',
+  },
+  {
+    image: 'slide4.png',
+    tagLine: 'Autocollants <span>avec découpe laser sur mesure</span>',
+  },
+];
+
+arrowRightElement.addEventListener('click', () => {
+  imgBannerElement.src = './assets/images/slideshow/slide2.jpg';
+  pBannerElement.innerHTML =
+    'Tirages haute définition grand format <span>pour vos bureaux et events</span>';
+});
